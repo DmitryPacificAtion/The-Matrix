435905f2b110f98861d2b5e076efa3244ff1d401
Dmitry Bruslik <dmitry.bruslik@yandex.ua>
2017-07-29 14:48:47 +0300

    Добавил много строк

--- the-matrix.js
+++ the-matrix.js
@@ -1,32 +1,36 @@
 var alphabet = 'ABCĆČDĐEFGHIJKLMNOPQRSŠTUVWXYZŽabcčćdđefghijklmnopqrsštuvwxyzžАБВГҐДЂЕЁЄЖЗЅИІЇЙЈКЛЉМНЊОПРСТЋУЎФХЦЧЏШЩЪЫЬЭЮЯабвгґдђеёєжзѕиіїйјклљмнњопрстћуўфхцчџшщъыьэюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψωάΆέΈέΉίϊΐΊόΌύΰϋΎΫὰάὲέὴήὶίὸόὺύὼώΏĂÂÊÔƠƯăâêôơưאבגדהוזחטיכךלמםנןסעפףצץקרשת1234567890?!(%)[#]{@}®©$€£¥¢₪';
 alphabet = alphabet.split('');
 var row = document.querySelectorAll('#row td');
-
-function ChinaString (v_stringLen = 500) {
+var tempArr = row;
+var index = 0;
+function ChinaString (index, v_stringLen = 500) {
 	var v_string = '';
+	var flagInterval = true;
+	this.index = index;
 	this.сhinaString = function() {
 		// debugger;
-		// var positionIndex = getRandomInt(0, 27);
-		row[1].innerHTML = getString();
+		row[index].innerHTML = getString();
+	}
+	this.setInt = function() {
+			setInterval(this.сhinaString, 100);
 	}
 	var getString = function () {
-		if (v_string.length < v_stringLen) 
+		if (v_string.length < v_stringLen){
 			return v_string += '<span>' + alphabet[getRandomInt(0, 299)] + '</span>';
+		}
 		else {
 			v_string = '';
 		}
 		return v_string;
 	}
 }
-
 function stringGenerator() {
-	// debugger;
-	return new ChinaString();
+	index = getRandomInt(0, tempArr.length-1);
+	return new ChinaString(index);
 }
-
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 var str = stringGenerator();
-setInterval('str.сhinaString()', 100);
+setInterval('stringGenerator().setInt()', 500);
